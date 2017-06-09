decd0aee9d350a0b8538127209a5526a1c0f2c92
carlloganjr <carlloganjr@gmail.com>
2017-06-09 00:27:24 -0700

    added lightbox plugin & media queries

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var searchPhotos = document.getElementById('search');
+var photoGallery = document.getElementsByClassName('photo-gallery');
+var photos = document.getElementsByTagName('title')[0].innerHTML;
+
+function searchInput() {
+
+}
