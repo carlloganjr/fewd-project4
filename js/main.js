0e43694e954c9bc54b95a033721f1cbf7871b32c
carlloganjr <carlloganjr@gmail.com>
2017-06-10 20:38:31 -0700

    updated javascript
    added 'no images found' for a search with no results

--- js/main.js
+++ js/main.js
@@ -8,6 +8,14 @@ function searchInput() {
       } else {
         $(this).show();
       }
+      var listQty = $('.hide').children().length;
+      var hiddenImgages = $('li').children(':hidden').length;
+      if(hiddenImgages === listQty) {
+        $('.no-img').html("<h1>No images found</h1>");
+      }
+      if(hiddenImgages < listQty) {
+        $('h1').empty();
+      }
     });
   });
 }
